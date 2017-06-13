097a2c45932257ca163b9690af50253105bbfab8
Jaesun Lee <janatee84@gmail.com>
2017-06-13 10:58:01 +0900

    Initial commit

---
+++ app/src/main/java/net/misociety/example/openweatherexample/MainActivity.java
@@ -0,0 +1,15 @@
+package net.misociety.example.openweatherexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
